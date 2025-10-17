81ec21e929544eefd80c89359bb3faf54969fcfe
Yashbhatia27 <yash278bhatia@gmail.com>
2025-10-17 20:17:04 +0530

    debugging pdp ue page

--- scripts/editor-support.js
+++ scripts/editor-support.js
@@ -13,7 +13,7 @@ import { decorateMain } from './scripts.js';
 
 window.xwalk = window.xwalk || {};
 window.xwalk.isAuthorEnv = true;
-window.xwalk.previewSku = 'ADB150';
+window.xwalk.previewSku = 'TOYOTA04';
 
 // set the filter for an UE editable
 function setUEFilter(element, filter) {
